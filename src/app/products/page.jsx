0c435410c017d562b3385c468f55eea0a3f7b2f4
alfu-git <mdalfaz.dev@gmail.com>
2026-04-21 14:34:47 +0600

    explore cache revalidate concept

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const getProducts = async () => {
   const res = await fetch("http://localhost:5000/products", {
-    cache: "no-store",
+    next: {revalidate: 10}
   });
   return res.json();
 };
